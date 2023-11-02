6ba3344114d95c30f252e688d9933024971a317b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-02 15:26:21 +0400

    install tailwind and clear inital code

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+</script>
+
+<main>
+  <h1 class="text-red-600">Hello from Svelte</h1>
+</main>
